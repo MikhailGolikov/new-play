9b0a472829caf49bbcc635255d2d553c4268caa2
Mikhail Golikov <misha-golikov2006@mail.ru>
2025-02-08 10:43:30 +0700

    добавил и считал переменные x,y

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ int main() {
     cout << "Hello, World!" <<endl;
     int a = 5;
     int b = 7;
-    cout << a + b;
-    return 0;
+    cout << a + b<<endl;
+
+
+    int x, y;
+
+    cin >> x >> y;
+
+
+
 }
